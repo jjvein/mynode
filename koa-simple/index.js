2c0a9b3269b07cfae30723e8f26acd6c936aa27a
jjvein <jjvein@gmail.com>
2016-04-12 11:40:33 +0800

    koa-stud y

--- koa-simple/index.js
+++ koa-simple/index.js
@@ -18,12 +18,11 @@ SimpleKoa.prototype = {
     var middlewares = ctx.middlewares
 
     return co(function* (){
-      var prev = (function*(){})()
+      var prev = (function*(){ })()
       var i = middlewares.length
       while(i--){
         prev = middlewares[i].call(ctx, prev)
       }
-
       yield prev
     })
 
@@ -34,22 +33,27 @@ var app = new SimpleKoa();
 
 app.use(function *(next){
     this.body = '1';
-    yield next;
+    yield (function* (a){this.body += a}).bind(this)('hello')
+    yield next
     this.body += '5';
     console.log(this.body);
+    return 'a'
 });
 app.use(function *(next){
     this.body += '2';
     yield next;
     this.body += '4';
+    return 'b'
 });
 app.use(function *(next){
     this.body += '3';
     yield next
+    return 'c'
 });
 app.use(function *(next){
   this.body += 'A'
   yield next
+  return 'd'
 })
-app.listen().then(value=> {console.log(this)})
+app.listen().catch(err=>{console.log(err)})
 

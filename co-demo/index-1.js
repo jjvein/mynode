bea19c9844d2ddda53f4ffb6eff5bad376e4b9c0
jjvein <jjvein@gmail.com>
2016-04-16 14:33:38 +0800

    koa co generator

--- co-demo/index-1.js
+++ co-demo/index-1.js
@@ -8,11 +8,15 @@ console.log(ret.then(value=>{lg(value)}))
 
 
 ret = co(function* (){
+  
+  var a = 10
+  var ret = yield [a]
+  var ret1 = yield {name: 'foo', age: 23}
 
-    var a = 10
-    var ret = yield [a]
+  var sum = ret[0] + ret1.age
 
-    return ret
+  return sum
 })
+
 lg(ret.then(value=> {lg(value)}))
 

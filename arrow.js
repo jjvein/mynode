fbf210ff8126d844525f10600e6ee10bbe5edccc
jjvein <jjvein@gmail.com>
2016-04-20 23:48:48 +0800

    arrow 箭头函数“

--- arrow.js
+++ arrow.js
@@ -24,3 +24,19 @@ let mary = new Student({
 
 console.log( mary.getInfo() ); // "Mary Lou, 13"
 mary.sayHi();
+
+var a = [
+  "Hydrogen",
+  "Helium",
+  "Lithium",
+  "Beryllium"
+];
+
+var a2 = a.map(function(s){ return s.length });
+
+//当只有一个参数时，可以省略括号，并且右侧如果只有一个语句，则是函数的返回值
+var a3 = a.map( s => s.length );
+console.log('a2: ', a2)
+console.log('a3: ', a3)
+
+

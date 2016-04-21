508273cc0835638bb99a73cad4599a9b6032ab4e
jjvein <jjvein@gmail.com>
2016-04-21 10:08:21 +0800

    arrow function usage

--- arrow1.js
+++ arrow1.js
@@ -32,4 +32,27 @@ l(adder.addCall(1))
 //并没有将函数的this绑定到obj上。
 
 
+//(() =>'footbar')()
 
+//return undefined
+var empty = () => {}
+l(empty())
+
+var simple = x => x > 15 ? 15 : x
+l(simple(16))
+l(simple(10))
+
+
+var max = (a,b) => a > b ? a : b
+
+l(max(3, 2))
+l(max(2, 3))
+
+var arr = [1, 2, 3, 4, 5]
+var sum = arr.reduce((a,b)=> a + b)
+var even = arr.filter(v=> v % 2 == 0)
+var dou =  arr.map(v => v * 2)
+
+l('sum', sum)
+l('even', even)
+l('dou', dou)

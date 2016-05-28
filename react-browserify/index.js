9e5dacbd1a63922588384aaf00fb2609eaf148c0
jjvein@gmail.com <jjvein@gmail.com>
2016-05-28 19:24:24 +0800

    react browserify babel preset es2015

--- react-browserify/index.js
+++ react-browserify/index.js
@@ -6,3 +6,17 @@ ReactDOM.render(
   <SearchForm />,
   document.querySelector('#placeholder')
 )
+
+
+//可以同时调用多个 render 方法
+setInterval(function(){
+  var cD = new Date()
+  var dateTime = "Last Sync: " + cD.getDate() + '/' + (cD.getMonth() + 1)
+    + '/' + cD.getFullYear() + '@' 
+    + cD.getHours() + ':'
+    + cD.getMinutes() + ':' + cD.getSeconds()
+  ReactDOM.render(
+    <div>{ dateTime }</div>,
+    document.querySelector('#datetime')
+  )
+}, 1000)
